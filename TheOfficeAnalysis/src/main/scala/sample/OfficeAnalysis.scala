750b371a2cf327dde68f876177475183dee3dfb6
kamath7 <adithyakamath96@gmail.com>
2023-07-12 17:38:58 +0530

    season 7 struck a chord

--- TheOfficeAnalysis/src/main/scala/sample/OfficeAnalysis.scala
+++ TheOfficeAnalysis/src/main/scala/sample/OfficeAnalysis.scala
@@ -1,5 +1,6 @@
 package sample
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{avg, col, regexp_replace, when, floor}
 
 object OfficeAnalysis {
 
@@ -7,6 +8,24 @@ object OfficeAnalysis {
     val spark = SparkSession.builder().appName("Office").master("local[*]").getOrCreate()
 
     val df = spark.read.option("header","true").csv("D:\\Code\\MachineLearning-Scripts\\TheOfficeAnalysis\\src\\main\\scala\\sample\\the_office_series.csv")
-    df.printSchema()
+//    df.printSchema()
+
+    import spark.implicits._
+
+    val cleanedUpDf = df.withColumn("season_cleaned", regexp_replace(col("Season"), "[^0-9.]", ""))
+      .withColumn("season_cleaned", when(col("season_cleaned").isNull, null)
+        .otherwise(floor(col("season_cleaned").cast("double") % 10)))
+      .filter(col("season_cleaned").isNotNull)
+
+    cleanedUpDf.show(truncate = false)
+
+    val episodeCountsBySeason = cleanedUpDf.groupBy("season_cleaned").count().orderBy("season_cleaned")
+    episodeCountsBySeason.show()
+
+    val averageRatingsBySeason = cleanedUpDf.groupBy("season_cleaned").agg(avg("ratings").alias("Average_Ratings"), avg("Viewership").alias("Average_Viewership"))
+    averageRatingsBySeason.show()
+
+
+    spark.stop()
   }
 }
