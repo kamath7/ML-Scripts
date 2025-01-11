e310ff690827764ddba36ce906fa7bd2ced7a3b7
Adithya Kamath <adithyakamath96@gmail.com>
2025-01-12 00:51:41 +0530

    model created

--- OMNX/app.py
+++ OMNX/app.py
@@ -2,12 +2,30 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np 
 import onnx
-import tf2onnx 
+import tf2onnx
+
 
 data = pd.read_csv("./Salary_Data.csv")
 
-x = data["YearsExperience"].values.reshape(-1,1)
+X = data["YearsExperience"].values.reshape(-1,1)
 y = data["Salary"].values
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=32 )
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1, input_dim = 1)
+])
+model.compile(optimizer="adam", loss="mse")
+
+model.fit(X_train_scaled, y_train, epochs=100)
+
 
 
 
